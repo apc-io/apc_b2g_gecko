47929591934638775945b505eff575f3f9b6051d
Nguyen Thanh Trung <nguyenthanh.trung@nomovok.vn>
2013-07-25 17:57:48 +0700

    Remap Backspace and Delete keys on keyboard to correct their behavior

--- widget/gonk/GonkKeyMapping.h
+++ widget/gonk/GonkKeyMapping.h
@@ -85,7 +85,7 @@ static const unsigned long kKeyMapping[] = {
     0, // EXPLORER
     0, // ENVELOPE
     NS_VK_RETURN, // ENTER
-    NS_VK_DELETE,
+    NS_VK_BACK, // BACKSPACE,
     NS_VK_BACK_QUOTE, // GRAVE
     NS_VK_HYPHEN_MINUS,
     NS_VK_EQUALS,
@@ -129,8 +129,8 @@ static const unsigned long kKeyMapping[] = {
     0, // BUTTON_START
     0, // BUTTON_SELECT
     0, // BUTTON_MODE
-    0, // ESCAPE
-    0, // FORWARD_DEL
+    NS_VK_ESCAPE,
+    NS_VK_DELETE,
     0, // CTRL_LEFT
     0, // CTRL_RIGHT
     NS_VK_CAPS_LOCK,
