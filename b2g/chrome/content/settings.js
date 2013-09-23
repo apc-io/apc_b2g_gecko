334d40b3ca67d4df4a3fc7d982e5e59a58973013
Nguyen Thanh Trung <nguyenthanh.trung@nomovok.vn>
2013-09-23 15:11:15 +0700

    changed software name + version and used ro.board.platform intead of ro.hardware for hardware revision/model

--- b2g/chrome/content/settings.js
+++ b2g/chrome/content/settings.js
@@ -206,7 +206,8 @@ Components.utils.import('resource://gre/modules/ctypes.jsm');
   let firmware_revision = null;
   let product_model = null;
 #ifdef MOZ_WIDGET_GONK
-    hardware_info = libcutils.property_get('ro.hardware');
+    // hardware_info = libcutils.property_get('ro.hardware');
+    hardware_info = libcutils.property_get('ro.board.platform');
     firmware_revision = libcutils.property_get('ro.firmware_revision');
     product_model = libcutils.property_get('ro.product.model');
 #endif
